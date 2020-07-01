bfc201ae0d178dbaa7fbbcf62dbe496a58ac66c1
Hadie Danker <hadie87@gmail.com>
2020-07-02 04:53:32 +0700

    add pages

---
+++ build/precache-manifest.e3eb2ab0bf85e88c2ec6673ffb087001.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "008100e9b4f006e94f9f39c86a5e8434",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c7a497a9b92a425b70ba",
+    "url": "/static/js/2.74f966bd.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.74f966bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ac57cdd4ac09ace47a00",
+    "url": "/static/js/main.802164cb.chunk.js"
+  },
+  {
+    "revision": "8aba14c5515e5362888a",
+    "url": "/static/js/runtime-main.66cf0057.js"
+  }
+]);
