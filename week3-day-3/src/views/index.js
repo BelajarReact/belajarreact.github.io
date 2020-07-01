dc9b054a0ae81a11a78b950c336994e870337757
Hadie Danker <hadie87@gmail.com>
2020-07-02 03:03:15 +0700

    remove all files

--- week3-day-3/src/views/index.js
+++
@@ -1,4 +0,0 @@
-export { default as About } from "./Tentang";
-export { default as Categories } from "./Categories";
-export { default as Home } from "./Home";
-export { default as Contact } from "./Contact";
